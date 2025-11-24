0661c847c0311c5c71443a98ed62495e281728c6
Mordret <dinu.plesu@isa.utm.md>
2025-11-24 18:31:50 +0200

    Task2: Added printing of results
    Used the println method  and provided the results.

--- lab-display-assistant/task2/src/task2/Main.java
+++ lab-display-assistant/task2/src/task2/Main.java
@@ -15,7 +15,17 @@ public class Main {
             try {
                 String content = FileReader.readFileIntoString(filePath);
             TextData textData = new TextData(filePath, content);
-             
+              System.out.println("----- Text Data Analysis -----");
+        System.out.println("File Name       : " + textData.getFileName());
+      
+        System.out.println("Vowels          : " + textData.getNumberOfVowels());
+      
+        System.out.println("Consonants      : " + textData.getNumberOfConsonants());
+        System.out.println("Total Letters   : " + textData.getNumberOfLetters());
+        System.out.println("Sentences       : " + textData.getNumberOfSentences());
+        //It is the first longest word found in the text
+        System.out.println("Longest Word    : " + textData.getLongestWord());
+        System.out.println("------------------------------");
             } catch (IOException e) {
                 System.err.println("Error reading file '" + filePath + "': " + e.getMessage());
             }
