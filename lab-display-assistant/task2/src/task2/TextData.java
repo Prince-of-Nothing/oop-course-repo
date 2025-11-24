18cfc452e2d0e48a8df41b0a58c13f0fe088a3f5
Mordret <dinu.plesu@isa.utm.md>
2025-11-24 17:54:42 +0200

    Task2: TextData.java creation
    Starting implementation of TextData class. All that exists is the declaration of the attributes , but not their processing.

---
+++ lab-display-assistant/task2/src/task2/TextData.java
@@ -0,0 +1,13 @@
+package task2;
+
+public class TextData {
+    //The private attributes
+    private String fileName;
+    private String text;
+    private int numberOfVowels;
+    private int numberOfConsonants;
+    private int numberOfLetters;
+    private int numberOfSentences;
+    private String longestWord;
+
+}
