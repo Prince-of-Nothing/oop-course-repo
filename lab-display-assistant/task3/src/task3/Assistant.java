57f6ebcce6c03eda17df63f7112034d6a9a9a7ae
Mordret <dinu.plesu@isa.utm.md>
2025-11-25 12:24:23 +0200

    Task 3: Added the shell of the class , and compiled the Display class
    Added to Assistant.java the shell methods and attributes
    Compiled Display.java with '
    javac -d out -cp out C:\Users\Unknown\Documents\GitHub\oop-course-repo\lab-display-assistant\task3\src\task3\Main.java'

---
+++ lab-display-assistant/task3/src/task3/Assistant.java
@@ -0,0 +1,36 @@
+package task3;
+import task1.Display;
+import java.util.List;
+
+public class Assistant {
+    private String assistantName;
+    private List<Display> assignedDisplays;
+
+    public Assistant(String assistantName) {
+    }
+
+    // Adds a Display object to the assignedDisplays list
+    public void assignDisplay(Display d) {
+    }
+
+    // Iterates through assignedDisplays, comparing each Display with every other Display
+    public void assist() {
+    }
+
+    // Removes a display from the list and returns it
+    public Display buyDisplay(Display d) {
+    }
+
+    // Returns the number of assigned displays
+    public int getAssignedDisplaysCount() {
+    }
+
+    // Returns the Display object at the specified index
+    public Display getDisplayByIndex(int index) {
+    }
+
+    // Lists all available displays for purchase
+    public void listAvailableDisplays() {
+        
+    }
+}
