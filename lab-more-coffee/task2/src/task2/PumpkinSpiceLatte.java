77343fd8b393eebe29155bf860941e87d635c2ea
Mordret <dinu.plesu@isa.utm.md>
2025-12-03 09:44:23 +0200

    Task 2. Better looking code placement
    Minor changes

--- lab-more-coffee/task2/src/task2/PumpkinSpiceLatte.java
+++ lab-more-coffee/task2/src/task2/PumpkinSpiceLatte.java
@@ -1,32 +1,30 @@
 package task2;
 
 public class PumpkinSpiceLatte extends Cappuccino {
-    private int mgOfPumpkinSpice;
     private final String name= "PumpkinSpiceLatte";
+    private int mgOfPumpkinSpice;
     private int mlOfMilk;
+
     public PumpkinSpiceLatte(Intensity intensityOfCoffee, int mlOfMilk, int mgOfPumpkinSpice) {
         super(intensityOfCoffee, mlOfMilk);
         this.mgOfPumpkinSpice = mgOfPumpkinSpice;
         this.mlOfMilk=mlOfMilk;
     }
 
-    public int getMgOfPumpkinSpice() {
-        return mgOfPumpkinSpice;
-    }
+    public int getMgOfPumpkinSpice() {return mgOfPumpkinSpice;}
 
-    public void setMgOfPumpkinSpice(int mgOfPumpkinSpice) {
-        this.mgOfPumpkinSpice = mgOfPumpkinSpice;
-    }
+    public void setMgOfPumpkinSpice(int mgOfPumpkinSpice) {this.mgOfPumpkinSpice = mgOfPumpkinSpice;}
+    
     @Override
     public int getMlOfMilk() { return mlOfMilk; }
+
     @Override
     public void setMlOfMilk(int mlOfMilk) { this.mlOfMilk = mlOfMilk; }
 
     @Override
-    public String getName() {
-        return name;
-    }
-     @Override
+    public String getName() {return name;}
+     
+    @Override
     public void printCoffeeDetails() {
         System.out.println("Coffee type: " + getName());
         System.out.println("Intensity:"+getCoffeeIntensity()+"Ingredients: Milk "+super.getMlOfMilk()+"mL"+", "+"Pumpkin Spice "+mgOfPumpkinSpice+"mg");
